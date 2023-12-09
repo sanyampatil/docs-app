cb49566dd643c901eb040b818e363fd6d4d8f8a0
sanyam <patilsanyam869@gmail.com>
2023-12-09 17:20:33 +0530

    reinitialize tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>
+      <h1 className=' relative w-full h-screen bg-zinc-800'>
+
+      <h1 className=' absolute text-[12vw]  top-1/2 left1/2 -translate-x-[50%] -translate-y-[50%]  leading-none traking-tighter'>docs</h1>
         app
       </h1>
     </div>
